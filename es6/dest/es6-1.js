0947bc562c62dd53819d054135f1d238071e08bb
tom <tom.zhu1119@gmail.com>
2016-02-18 22:44:33 +0800

    es6

---
+++ es6/dest/es6-1.js
@@ -0,0 +1,17 @@
+//let
+"use strict";
+
+var a = 10;
+var b = 1;
+console.log(a);
+console.log(b);
+
+var i = 100;
+for (var _i = 0; _i < 10; _i++) {
+  console.log(_i);
+}
+console.log(i);
+
+var PI = 3.1415;
+//PI = 3; // 常规模式时，重新赋值无效，但不报错
+//PI // 3.1415
---
+++ es6/es6-1.js
@@ -0,0 +1,19 @@
+'use strict'
+//let
+let a = 10;
+var b = 1;
+console.log(a);
+console.log(b);
+
+var i=100;
+for(let i=0;i<10;i++){
+  console.log(i);
+}
+console.log(i);
+
+const PI = 3.1415;
+//PI = 3; // 常规模式时，重新赋值无效，但不报错
+//PI // 3.1415
+//let [foo, [[bar], baz]] = [1, [[2], 3]];
+
+
