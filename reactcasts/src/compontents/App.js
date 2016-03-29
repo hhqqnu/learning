a81b51a50e3864280aa2eb2044ce8e3ea8a7fc88
tom <tom.zhu1119@gmail.com>
2016-03-29 23:11:02 +0800

    webpack

---
+++ reactcasts/src/compontents/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+export default class Appe extends React.Component {
+  render(){
+    return (
+        <div className={styles.container}>
+          <h1 className={styles.title}>Hello World!</h1>
+        </div>
+    );
+  }
+}
